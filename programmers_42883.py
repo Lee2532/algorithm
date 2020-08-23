97807083e4f1be913640155e49e54509cff70bf7
pas time <seungoh0423@gmail.com>
2020-08-23 16:50:00 +0900

    release

---
+++ programmers_42883.py
@@ -0,0 +1,13 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42883
+
+def solution(number, k):
+    result = []
+    for i in number:
+        print(i)
+    # print(list(number))
+
+
+if __name__ == '__main__':
+    number = "1231234"
+    k = 3
+    solution(number, k)
---
+++ programmers_49189.py
@@ -0,0 +1,14 @@
+#https://programmers.co.kr/learn/courses/30/lessons/49189
+
+from collections import defaultdict
+
+def solution(n, edge):
+    print(n,edge)
+    answer = 0
+    return answer
+
+if __name__ == '__main__':
+    n = 6
+    edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+
+    print(solution(n, edge))
